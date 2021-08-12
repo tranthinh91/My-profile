92dd5ecde6b2a8f5ad8631759390b91825f322cb
tranthinh91 <thinhtran@vn.hondaplus.co.jp>
2021-08-12 16:34:41 +0700

    index.html

---
+++ js.js
@@ -0,0 +1,40 @@
+$(function() {
+
+    $('#ac1').click(function() {
+        $('#Introduce').show();
+        $('#Education').hide();
+        $('#Experience').hide();
+        $('#Hobbies').hide();
+    })
+
+});
+$(function() {
+
+    $('#ac2').click(function() {
+        $('#Introduce').hide();
+        $('#Education').show();
+        $('#Experience').hide();
+        $('#Hobbies').hide();
+    })
+
+});
+$(function() {
+
+    $('#ac3').click(function() {
+        $('#Introduce').hide();
+        $('#Education').hide();
+        $('#Experience').show();
+        $('#Hobbies').hide();
+    })
+
+});
+$(function() {
+
+    $('#ac4').click(function() {
+        $('#Introduce').hide();
+        $('#Education').hide();
+        $('#Experience').hide();
+        $('#Hobbies').show();
+    })
+
+});
